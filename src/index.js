ab43621a5fc3ba16d6af5b5af858c140aeef7a57
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-28 17:24:05 +0300

    routes.js added, home path added to routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { Router, browserHistory } from "react-router";
 
-import App from './components/app';
-import reducers from './reducers';
+import routes from "./routes";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.querySelector(".container")
+);
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Route, IndexRoute } from "react-router";
+import App from "./components/App";
+
+export default <Route component={App} path="/" />;
