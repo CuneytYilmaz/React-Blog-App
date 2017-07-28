cb0cc10699352a49121904636e00665af60df21d
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-28 19:19:51 +0300

    Lifecycle method added to Posts Index component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions/index";
 
-export default () => {
-  return <div>List of blog posts.</div>;
-};
+class PostsIndex extends Component {
+  // This lifecycle method runs only this component render to DOM
+  componentWillMount() {
+    console.log("now");
+    this.props.fetchPosts();
+  }
+
+  render() {
+    return <div>A list of blog posts.</div>;
+  }
+}
+
+export default connect(null, { fetchPosts })(PostsIndex);
