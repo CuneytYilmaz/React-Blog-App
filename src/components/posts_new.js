8ed39f0b3d9ff68a65c9116d951c113996b767ef
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-28 23:57:37 +0300

    Redux form controlls added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,20 +3,21 @@ import { reduxForm } from "redux-form";
 
 class PostsNew extends Component {
   render() {
+    const { handleSubmit, fields: { title, categories, content } } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <h3>Create A New Post</h3>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...title} />
         </div>
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" {...categories} />
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control" />
+          <textarea className="form-control" {...content} />
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
