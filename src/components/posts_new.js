dda1577500b196d1dafb6ab680257d83f5ee6f83
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-29 01:28:06 +0300

    Validation for all inputs has completed via redux-form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -5,23 +5,42 @@ import { createPost } from "../actions/index";
 class PostsNew extends Component {
   render() {
     const { handleSubmit, fields: { title, categories, content } } = this.props;
+    console.log(title.error);
     return (
       <form onSubmit={handleSubmit(this.props.createPost)}>
         <h3>Create A New Post</h3>
-        <div className="form-group">
+        <div
+          className={`form-group ${title.touched && title.invalid
+            ? "has-danger"
+            : ""}`}
+        >
           <label>Title</label>
           <input type="text" className="form-control" {...title} />
-          <div>
+          <div className="text-help">
             {title.touched ? title.error : ""}
           </div>
         </div>
-        <div className="form-group">
+        <div
+          className={`form-group ${categories.touched && categories.invalid
+            ? "has-danger"
+            : ""}`}
+        >
           <label>Categories</label>
           <input type="text" className="form-control" {...categories} />
+          <div className="text-help">
+            {categories.touched ? categories.error : ""}
+          </div>
         </div>
-        <div className="form-group">
+        <div
+          className={`form-group ${content.touched && content.invalid
+            ? "has-danger"
+            : ""}`}
+        >
           <label>Content</label>
           <textarea className="form-control" {...content} />
+          <div className="text-help">
+            {content.touched ? content.error : ""}
+          </div>
         </div>
         <button type="submit" className="btn btn-primary">
           Submit
@@ -31,10 +50,16 @@ class PostsNew extends Component {
   }
 }
 
-function validate(props) {
+function validate(values) {
   const errors = {};
-  if (!props.title) {
+  if (!values.title) {
     errors.title = "Enter a username";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content";
   }
   return errors;
 }
