57ef2492d1190df61546ba868b1d6aed082c8c00
Cüneyt Yılmaz <cuneyilmaz@gmail.com>
2017-07-28 17:59:31 +0300

    PostsIndex component added, Index route connected to routes.js

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { Component } from 'react';
+import React from "react";
+import { Component } from "react";
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        React simple starter
+        {this.props.children}
+      </div>
     );
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,18 @@
 import React from "react";
 import { Route, IndexRoute } from "react-router";
-import App from "./components/App";
 
-export default <Route component={App} path="/" />;
+import App from "./components/App";
+import PostsIndex from "./components/posts_index";
+
+const Greeting = () => {
+  return <div>Hi this is children component.</div>;
+};
+
+export default (
+  <Route component={App} path="/">
+    <IndexRoute component={PostsIndex} />
+    <Route path="greet" component={Greeting} />
+    <Route path="greet2" component={Greeting} />
+    <Route path="greet3" component={Greeting} />
+  </Route>
+);
